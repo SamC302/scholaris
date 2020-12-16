b640a33f0569e60b322c88eb0d9f04d179ab4385
Kandasamy Chokkalingam <samchoks@hotmail.com>
2020-12-15 21:17:20 -0500

    Initialization and add Grade enum

---
+++ Grades.py
@@ -0,0 +1,37 @@
+import enum
+
+
+class Grade(enum.Enum):
+    A = 5
+    B = 4
+    C = 3
+    D = 2
+    F = 1
+
+    def __gt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value > other.value
+        elif other.__class__ is int:
+            return self.value > other
+        return NotImplemented
+
+    def __ge__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value >= other.value
+        elif other.__class__ is int:
+            return self.value >= other
+        return NotImplemented
+
+    def __lt__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value < other.value
+        elif other.__class__ is int:
+            return self.value < other
+        return NotImplemented
+
+    def __le__(self, other):
+        if self.__class__ is other.__class__:
+            return self.value <= other.value
+        elif other.__class__ is int:
+            return self.value <= other
+        return NotImplemented
